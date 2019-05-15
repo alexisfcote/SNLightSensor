f56e07f16e9f07eed4cf90779bc34314ef1816e5
Alexis Fortin-Côté <ALFOC@ulaval.ca>
2019-05-15 12:51:50 -0400

    Updated pectral monitor to show values and save to disk

--- Sensor AS7262 streaming/spectral_monitor.py
+++ Sensor AS7262 streaming/spectral_monitor.py
@@ -32,6 +32,8 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._main = QtWidgets.QWidget()
         self.setCentralWidget(self._main)
         self.setWindowTitle("Spectral Monitor")
+        self.file = None
+
         layout = QtWidgets.QHBoxLayout(self._main)
         adafruit_ser_name = None
         while adafruit_ser_name is None:
@@ -42,8 +44,32 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
         intensity_canvas = FigureCanvas(Figure(figsize=(3, 3)))
+
+        values_grid   = QtWidgets.QGridLayout()
+        values_gbox   = QtWidgets.QGroupBox("Values")
+        values_labels = []
+        self.values_edits  = []
+        for id, val in enumerate(value_col):
+            values_labels.append(QtWidgets.QLabel(val))
+            self.values_edits.append(QtWidgets.QTextEdit('NaN'))
+            self.values_edits[-1].setReadOnly(True)
+            self.values_edits[-1].setFixedHeight(25)
+
+            values_grid.addWidget(values_labels[-1], id, 0)
+            values_grid.addWidget(self.values_edits[-1], id, 1)
+            values_grid.setRowStretch(id, 0)
+        
+        values_grid.setRowStretch(id+1, 1)
+        button = QtWidgets.QPushButton('Stream to file')
+        button.clicked.connect(self.stream_to_file)
+        values_grid.addWidget(button, id+2, 0)
+        values_grid.setRowStretch(id+2, 0)
+
+        values_gbox.setLayout(values_grid)
+
         layout.addWidget(dynamic_canvas, 3)
         layout.addWidget(intensity_canvas, 1)
+        layout.addWidget(values_gbox, 1)
 
 
         self._dynamic_ax = dynamic_canvas.figure.subplots()
@@ -54,6 +80,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._timer = dynamic_canvas.new_timer(
             0, [(self._update_canvas, (), {})])
         self._timer.start()
+
+    def stream_to_file(self):
+        fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Save file',
+            '', "Text File (*.txt)")
+        if fname:
+            self.file = open(fname[0], 'w')
 
     def _update_canvas(self):
         self._dynamic_ax.clear()
@@ -70,9 +102,16 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self._intensity_ax.set_ylim([0, 15])
         self._intensity_ax.figure.canvas.draw()
 
+        for val, edit in zip(values, self.values_edits):
+            edit.setText("{}".format(val))
+
+        if self.file is not None:
+            self.file.write(time.strftime("%a, %d %b %Y %H:%M:%S,") + line.decode("utf-8"))
 
     def __exit__(self):
         self.ser.close()
+        if self.file is not None:
+            self.file.close()
 
 
 if __name__ == "__main__":
